deb0960615bb87a471b7a8de51afd1432a2a87f5
Isabela de Ponte <isabelapnt01@gmail.com>
2024-01-24 23:16:16 -0300

    :sparkles: feat: add showPassword at input field

--- src/components/FloatInput/FloatInput.jsx
+++ src/components/FloatInput/FloatInput.jsx
@@ -1,13 +1,35 @@
+import { useState } from 'react'
+
 import { InputDiv } from "./StyledFloatInput"
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 import './FloatInput_styles.css'
 
 export default function FloatInput({label, type, classes}) {
-  return (
+  
+const [showPassword, setShowPassword] = useState(false)
+
+const handleClickShowPassword = () => {
+  setShowPassword(!showPassword)
+}
+
+return (
     <>
-      <InputDiv className="label" >
-        <input type={type} placeholder=" " className={classes} />
-        <label>{label}</label>
+      <InputDiv className="label" >     
+        {
+          type == "password" ? 
+          <div>
+                <span onClick={handleClickShowPassword} >{!showPassword ? <FaEye /> : <FaEyeSlash />}</span>
+                <input type={showPassword ? "text" : type} placeholder=" " className={classes} />
+                <label>{label}</label>
+          </div> : 
+
+          <div>
+            <input type={type} placeholder=" " className={classes} />
+            <label>{label}</label>
+          </div>
+        }
+        
       </InputDiv>
     </>
   );

f084665b42b0464e40f7c7f377c418090859c7d8
carneiromatheu <carneiromatheu@gmail.com>
2024-01-26 13:40:11 -0300

    :lipstick: fix: added img accessibility

--- src/components/ProfileStamp/index.jsx
+++ src/components/ProfileStamp/index.jsx
@@ -4,7 +4,10 @@ function ProjectStamp() {
   return (
     <ProfileStampSC>
       <div>
-        <img src="/src/assets/images/profile-picture-default.svg" alt="" />
+        <img
+          src="/src/assets/images/profile-picture-default.svg"
+          alt="imagem de perfil"
+        />
       </div>
       <div>
         <h1>Lorem ipsum</h1>

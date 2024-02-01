62c1f796209a23a78ce5dafc9967da38c839b348
carneiromatheu <carneiromatheu@gmail.com>
2024-02-01 19:54:45 -0300

    :lipstick: FIX: Adjusted style

--- src/components/Dashboard/style.jsx
+++ src/components/Dashboard/style.jsx
@@ -3,19 +3,17 @@ import styled from "styled-components";
 import { bp } from "../../utils/breakpoints";
 
 export const DashboardSC = styled.main`
-  max-width: 1280px;
+  max-width: 80rem;
   min-height: 100dvh;
-  margin: 0 auto;
-  padding-top: 4.5rem;
+  
+  margin: 0rem auto;
+  padding: 4.5rem 2rem;
 
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  @media (max-width: ${bp.desktop}) {
-    padding-inline: 2rem;
-  }
   @media (max-width: ${bp.mobile}) {
     padding-inline: 1.5rem;
   }

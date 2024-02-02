25e73d6f07692201cf26a545973a18f18eb403a3
carneiromatheu <carneiromatheu@gmail.com>
2024-02-02 19:17:25 -0300

    :construction: breakpoint: getProjectImage, fixme

--- src/services/projectServices.js
+++ src/services/projectServices.js
@@ -22,3 +22,17 @@ export const getAuthUserProjects = async (userId) => {
   const response = await getJustOneUserWithYourProjectsByUserId(userId);
   return response.lstProjeto;
 };
+
+export const getProjectImage = async (projectId) => {
+  try {
+    const response = await api.post(`/projeto/${projectId}/download`);
+    console.log(response.data); // FIXME: APAGAR LINHA
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Erro ao obter imagem do projeto:",
+      error.response.status,
+      error.response.statusText
+    );
+  }
+};

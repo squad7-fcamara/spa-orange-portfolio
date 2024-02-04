2cac5654f931cfb3ff087b89cc17e15394cf0f04
carneiromatheu <carneiromatheu@gmail.com>
2024-02-04 00:07:58 -0300

    :bug: fix: bugfix auth

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,81 +1,100 @@
 import { api } from "../../api/apiRest";
 
 import { NavLink } from "react-router-dom";
-import toast, { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from "react-hot-toast";
 
-
-import LoginGoogleBtn from "../../components/LoginGoogleBtn"
-import { FormLogin, AppContainer, ImageContainer, HeroImage, ContentContainer, Title, Cadastro, LinkCadastro } from "./StyledLogin"
+import LoginGoogleBtn from "../../components/LoginGoogleBtn";
+import {
+  FormLogin,
+  AppContainer,
+  ImageContainer,
+  HeroImage,
+  ContentContainer,
+  Title,
+  Cadastro,
+  LinkCadastro,
+} from "./StyledLogin";
 import PrimaryButton from "../../components/PrimaryButton";
 
 import FloatInput from "../../components/FloatInput/FloatInput";
 
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
-
-  const onSubmit =  async (data) => {
-    console.log(data)
-
+  const onSubmit = async (data) => {
     try {
-      const response = await api.get(`usuario/validarLogin?email=${data.email}&senha=${data.senha}`);
-      if (response.data === -1) {
-        console.log('Login falhou:', response.data);
-        toast.error('Email ou senha incorreta. Tente novamente.', {
+      const response = await api.get(
+        `usuario/validarLogin?email=${data.email}&senha=${data.senha}`
+      );
+      if (response.data.idUsuario === -1) {
+        console.log("Login falhou:", response.data);
+        toast.error("Email ou senha incorreta. Tente novamente.", {
           iconTheme: {
-            primary: '#fff',
-            secondary: '#DD0000',
+            primary: "#fff",
+            secondary: "#DD0000",
           },
           style: {
             background: "#DD0000",
             color: "white",
             minWidth: "20rem",
-          }
+          },
         });
       } else {
-        console.log('Login bem-sucedido:', response.data);
+        console.log("Login bem-sucedido:", response.data);
       }
     } catch (error) {
-      console.error('Erro na requisição:', error);
+      console.error("Erro na requisição:", error);
     }
-  }
-
+  };
 
   return (
     <AppContainer>
-
       <ImageContainer>
         <HeroImage />
       </ImageContainer>
 
       <ContentContainer>
-
-        
-          <Toaster
-            containerClassName="alert"
-            containerStyle={{
-              position: 'absolute',
-              marginTop: '2.5rem',
-              marginBottom: '1rem',
-            }}
-          />
-        
+        <Toaster
+          containerClassName="alert"
+          containerStyle={{
+            position: "absolute",
+            marginTop: "2.5rem",
+            marginBottom: "1rem",
+          }}
+        />
 
         <Title>Entre no Orange Portfólio</Title>
 
         <FormLogin onSubmit={handleSubmit(onSubmit)} method="get">
-
           <LoginGoogleBtn />
 
-          <FloatInput label={"email"} type={"email"} name={"email"} id_value={"email"} register={register} required={true} classes={errors.email && "required"} />
-          <FloatInput label={"senha"} type={"password"} name={"password"} id_value={"password"} register={register} required={true} classes={errors.senha && "required"} />
+          <FloatInput
+            label={"email"}
+            type={"email"}
+            name={"email"}
+            id_value={"email"}
+            register={register}
+            required={true}
+            classes={errors.email && "required"}
+          />
+          <FloatInput
+            label={"senha"}
+            type={"password"}
+            name={"password"}
+            id_value={"password"}
+            register={register}
+            required={true}
+            classes={errors.senha && "required"}
+          />
 
-          <PrimaryButton text={"ENTRAR"} ></PrimaryButton>
-
+          <PrimaryButton text={"ENTRAR"}></PrimaryButton>
         </FormLogin>
-
 
         <Cadastro>
           <NavLink to={"/sign-up"}>
@@ -83,10 +102,8 @@ const Login = () => {
           </NavLink>
         </Cadastro>
       </ContentContainer>
-
-
     </AppContainer>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,123 +1,144 @@
-import { SignUpContainer, Image, Title, NameColumn, AppContainer } from "./StyledSignUp"
+import {
+  SignUpContainer,
+  Image,
+  Title,
+  NameColumn,
+  AppContainer,
+} from "./StyledSignUp";
 
-import './SignUp_styles.css'
+import "./SignUp_styles.css";
 
-import myImage from '../../assets/img/img-signUp/img_cadastro.png'
-import FloatInput from "../../components/FloatInput/FloatInput"
-import PrimaryButton from "../../components/PrimaryButton"
+import myImage from "../../assets/img/img-signUp/img_cadastro.png";
+import FloatInput from "../../components/FloatInput/FloatInput";
+import PrimaryButton from "../../components/PrimaryButton";
 
-import toast, { Toaster } from 'react-hot-toast'
-import { useForm } from "react-hook-form"
+import toast, { Toaster } from "react-hot-toast";
+import { useForm } from "react-hook-form";
 
-import { api } from "../../api/apiRest"
+import { api } from "../../api/apiRest";
 
+export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-export default function SignUp(){
+  const notifyAlert = (status) => {
+    if (status == 200) {
+      toast.success("Cadastro feito com sucesso!", {
+        duration: 5000,
 
-    const { register, handleSubmit, formState: { errors } } = useForm()
+        iconTheme: {
+          primary: "#fff",
+          secondary: "#2E7D32",
+        },
 
+        style: {
+          background: "#2E7D32",
+          color: "white",
+          minWidth: "20rem",
+        },
+      });
+      return;
+    }
 
-    const notifyAlert = (status) => {
+    toast.error("Erro ao realizar o cadastro. Tente novamente", {
+      iconTheme: {
+        primary: "#fff",
+        secondary: "#DD0000",
+      },
 
-        if (status == 200) {
-            toast.success('Cadastro feito com sucesso!', {
+      style: {
+        background: "#DD0000",
+        color: "white",
+        minWidth: "20rem",
+      },
+    });
+  };
 
-                    duration: Infinity,
-        
-                    iconTheme: {
-                        primary: '#fff',
-                        secondary: '#2E7D32',
-                    },
-            
-                    style: {
-                        background: "#2E7D32",
-                        color: "white",
-                        minWidth: "20rem",
-                    }
-                }
-            )
-            return
-        }
+  const onSubmit = (data) => {
+    api
+      .post("usuario/", {
+        nome: data?.nome,
+        sobrenome: data?.sobrenome,
+        email: data?.email,
+        senha: data?.senha,
+      })
+      .then((response) => {
+        notifyAlert(response.status);
+      })
+      .catch((error) => {
+        notifyAlert(error.status);
+      });
+  };
 
-        toast.error('Erro ao realizar o cadastro. Tente novamente', {
+  return (
+    <AppContainer>
+      <Image src={myImage} />
 
-            iconTheme: {
-                primary: '#fff',
-                secondary: '#DD0000',
-            },
-    
-            style: {
-                background: "#DD0000",
-                color: "white",
-                minWidth: "20rem",
-            }
+      <SignUpContainer>
+        <div className="div-positioner">
+          <form
+            className="form"
+            onSubmit={handleSubmit(onSubmit)}
+            method="POST"
+          >
+            <Toaster
+              containerClassName="alert"
+              containerStyle={{
+                position: "absolute",
+                marginTop: "2.5rem",
+                marginBottom: "1rem",
+              }}
+            />
 
-            }
+            <Title>Cadastre-se</Title>
 
-        )
+            <NameColumn>
+              <FloatInput
+                label={"nome"}
+                type={"text"}
+                name={"firstName"}
+                register={register}
+                required={true}
+                classes={errors.nome && "required"}
+              />
+              <FloatInput
+                label={"sobrenome"}
+                type={"text"}
+                name={"lastName"}
+                register={register}
+                required={true}
+                classes={errors.sobrenome && "required"}
+              />
+            </NameColumn>
 
+            <FloatInput
+              label={"email"}
+              type={"email"}
+              name={"email"}
+              classes={errors.email && "required"}
+              register={register}
+              required={true}
+            />
+            <FloatInput
+              label={"senha"}
+              type={"password"}
+              name={"password"}
+              classes={errors.senha && "required"}
+              register={register}
+              required={true}
+            />
 
-    } 
+            {errors.senha && (
+              <span className="helperMessage">{errors.senha.message}</span>
+            )}
 
-    const onSubmit = (data) => {
-        
-        api.post('usuario/', {
-            nome: data?.nome,
-            sobrenome: data?.sobrenome,
-            email: data?.email,
-            senha: data?.senha
-        })
-        .then((response) => {
-            notifyAlert(response.status)
-        })
-        .catch((error) => {
-            notifyAlert(error.status)
-        }) 
-
-    };
-
-
-    return(
-        <AppContainer>
-            <Image src={myImage} />
-
-            <SignUpContainer>
-
-                <div className="div-positioner">
-
-                    <form className="form" onSubmit={handleSubmit(onSubmit)} method="POST" >
-
-
-                        <Toaster 
-                        containerClassName="alert"
-                            containerStyle={{
-                                position: 'absolute',
-                                marginTop: '2.5rem',
-                                marginBottom: '1rem',
-                                }}
-                        />
-
-                        <Title>Cadastre-se</Title>
-
-                        <NameColumn>
-                            <FloatInput label={"nome"} type={"text"} name={"firstName"} register={register} required={true} classes={errors.nome && "required"} />
-                            <FloatInput label={"sobrenome"} type={"text"} name={"lastName"} register={register} required={true} classes={errors.sobrenome && "required"} />
-                        </NameColumn>
-
-                        <FloatInput label={"email"} type={"email"} name={"email"} classes={errors.email && "required"} register={register} required={true} />
-                        <FloatInput label={"senha"} type={"password"} name={"password"} classes={errors.senha && "required"} register={register} required={true} />
-
-                        { errors.senha && <span className="helperMessage">{errors.senha.message}</span> }    
-                        
-                        <PrimaryButton text={"CADASTRAR"} />
-
-                    </form>
-
-                </div>
-
-
-            </SignUpContainer>
-
-        </AppContainer>
-    )
+            <PrimaryButton text={"CADASTRAR"} />
+          </form>
+        </div>
+      </SignUpContainer>
+    </AppContainer>
+  );
 }

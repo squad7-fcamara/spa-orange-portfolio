e28fa9cb7e18e814ccbf42acd12c7e1eec27fe4d
Isabela de Ponte <isabelapnt01@gmail.com>
2024-02-05 14:02:44 -0300

    fix: primary button image

--- src/components/PrimaryButton/PrimaryButton.jsx
+++ src/components/PrimaryButton/PrimaryButton.jsx
@@ -1,17 +1,9 @@
-import loadingGif from "../../assets/images/loading2.gif";
 import { ButtonPrimary } from "./StyledPrimaryButton";
 
-const PrimaryButton2 = ({ type = "button", onClick, text, loading }) => {
+const PrimaryButton2 = ({ type = "button", onClick, text}) => {
   return (
     <>
-      <ButtonPrimary type={type} onClick={onClick} disabled={loading}>
-          <img
-            className="loadingIcon"
-            src={loadingGif}
-            alt="Loading"
-            width="20"
-            height="20"
-          />
+      <ButtonPrimary type={type} onClick={onClick}>
           {text}
       </ButtonPrimary>
     </>
